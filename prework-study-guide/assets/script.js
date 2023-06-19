d0240a8fcd824b6b65b24d839f0f8226904cbb6e
PatK <pkykev@gmail.com>
2023-06-19 13:14:49 -0500

    added js to project

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,22 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for(x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+}
+
+
+
+
+/*
+if (topic === 'HTML') {
+    console.log ("Lets study with HTML!");
+} else if (topic === 'CSS') {
+    console.log("Lets study CSS!");
+} else if (topic === 'Git') {
+    console.log("Lets study with Git!");
+} else if (topic ==='JavaScript') {
+    console.log ("Lets study with JavaScript!");
+} else {
+    console.log('Please try again!')
+}
+*/
